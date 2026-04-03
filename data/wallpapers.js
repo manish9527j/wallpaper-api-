9d90c9c062544367b04bb74e191a1982dda0b2eb
Manish Jaiswal <manishjai952@gmail.com>
2026-04-03 13:09:13 +0530

    Create wallpapers.js with categories and placeholders
    Added initial structure for wallpapers data including categories and a placeholder for wallpapers.

---
+++ data/wallpapers.js
@@ -0,0 +1,21 @@
+// Copy your JSON data here
+export const wallpapersData = {
+  "Categories": [
+    {"name": "Abstract"},
+    {"name": "Amoled"},
+    {"name": "Couples"},
+    {"name": "Digital Art"},
+    {"name": "Material"},
+    {"name": "Minimal"},
+    {"name": "Scenic"},
+    {"name": "Space"},
+    {"name": "Superheroes"},
+    {"name": "Typography"}
+  ],
+  "Wallpapers": [
+    // Yahan apne saare wallpapers copy karo from your JSON file
+    // Example:
+    {"author": "BitSplash", "url": "https://raw.githubusercontent.com/sumitkolhe/Resources/master/wallpapers/Abstract/001.jpg", "thumbUrl": "https://raw.githubusercontent.com/sumitkolhe/Resources/master/thumbnails/Abstract/001.webp", "category": "Abstract"},
+    // ... rest of your 700+ wallpapers
+  ]
+};
